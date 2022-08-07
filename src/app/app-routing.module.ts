cf35d73f823ced74b0f1efe61a3c9766bc1b321a
thanapon-sam <samnatnp23@gmail.com>
2022-08-07 18:55:20 +0700

    Update project 07/08/56 | 18:54

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/home/login/login.component';
 
 const routes: Routes = [
   {
