52318f05e58ae8f95b47a9dcf7a21a8f5d2c1a6f
thanapon-sam <samnatnp23@gmail.com>
2022-08-17 01:08:31 +0700

    Update project 17/08/22 | 01:08

--- src/app/modules/admin/admin-booking/admin-booking.component.ts
+++ src/app/modules/admin/admin-booking/admin-booking.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../admin.service';
 import Swal from 'sweetalert2'
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { AdminBookingService } from './admin-booking.service';
 
 @Component({
   selector: 'app-admin-booking',
@@ -10,8 +12,34 @@ import { Router } from '@angular/router';
 })
 export class AdminBookingComponent implements OnInit {
 
-  constructor(private adminService: AdminService, private router: Router
-    ) { }
+  searchText: any;
+  listBooking: any;
+  page = 1;
+  count = 0;
+  tableSize = 10;
+  tableSizes = [3, 6, 9, 12];
+
+  submitted = false;
+
+  //
+  BookingForm = this.fb.group({
+    bookId: [''],
+    bookPre: [''],
+    bookDate: [''],
+    foodId: [''],
+    tableId: [''],
+    custId: [''],
+    // cateId: ['', Validators.required],
+
+  });
+
+  constructor(
+    private adminBookingService: AdminBookingService,
+    private adminService: AdminService,
+    private router: Router,
+    private fb: FormBuilder,
+    private activatedroute: ActivatedRoute,
+  ) { }
 
   booking: any = [];
 
@@ -19,23 +47,61 @@ export class AdminBookingComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.adminService.getBooking().subscribe(res => {
+    this.getAllProduct()
 
-      console.log('res =>', res)
-      if (res) {
-        this.booking = res;
+  }
+
+  getAllProduct() {
+    this.adminService.getBooking().subscribe(
+      (res) => {
+        console.log(res)
+        this.listBooking = res;
+      },
+      (error) => {
+        console.log(error);
       }
- 
+    );
+  }
 
-    });
-
+  onSubmit() {
+    console.log(this.BookingForm.value);
+    
+    this.submitted = true;
+    console.log('data :', this.BookingForm.value)
+    // stop here if form is invalid
+    if (this.BookingForm.invalid) {
+      Swal.fire({
+        icon: 'error',
+        title: 'กรุณากรอกข้อมูลให้ถูกต้อง',
+        text: 'Something went wrong!',
+      })
+    }else {
+      Swal.fire({
+        icon: 'success',
+        title: 'แก้ไขข้อมูลสินค้าสำเร็จ',
+        text: '',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.adminBookingService.updateBooking(this.BookingForm.value).subscribe(res => {
+            console.log('Create User res : ', res)
+            window.location.reload()
+          }
+          )
+        }
+      })
+    }
   }
 
   onEdit(item: any) {
-    alert('edit =>'+ item.bookId)
+    alert('edit =>' + item.bookId)
   }
 
   onDelete(item: any) {
+    let bookId = this.listBooking.bookId;
+    console.log(bookId);
+    console.log(item.bookId);
+
+
     Swal.fire({
       title: 'ต้องการลบข้อมูล?',
       text: 'คุณต้องการลบข้อมูลใช่หรือไม่!',
@@ -46,13 +112,23 @@ export class AdminBookingComponent implements OnInit {
       confirmButtonText: 'ยืนยัน',
     }).then((result) => {
       if (result.isConfirmed) {
-        this.adminService.deleteBooking(Number(item.bookId)).subscribe((res) => {
-          Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
-          this.ngOnInit();
-        });
+        this.adminService.deleteBooking(item.bookId).subscribe(
+          (res) => {
+            console.log(res);
+            Swal.fire('เรียบร้อย!', 'คุณได้ทำการลบข้อมูลสินค้าเรียบร้อย', 'success');
+            setTimeout(function () { window.location.reload(); }, 2 * 1000);
+          },
+          (error) => {
+            console.log('delete Product error : ', error);
+          });
       }
     });
   }
-  
+
+  refresh() {
+    this.getAllProduct()
+    // this.fetchData();
+    window.location.reload();
+  }
 
 }
--- src/app/modules/admin/admin-booking/admin-booking.service.ts
+++ src/app/modules/admin/admin-booking/admin-booking.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+const API_ENOPOINT = environment.API_ENOPOINT;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminBookingService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  updateBooking(updateBooking: any): Observable<any> {
+    console.log(updateBooking);
+    let bookId = updateBooking.bookId
+    console.log(bookId)
+    return this.http.put<any>(API_ENOPOINT + '/booking/update' ,JSON.stringify(updateBooking),httpOptions)
+    // return this.http.put<any>(API_ENOPOINT + '..' ,JSON.stringify(updateBooking),httpOptions)
+
+  }
 }
